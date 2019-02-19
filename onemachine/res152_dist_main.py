20374f4ee9b8d6fe914c1e37e109516939e1e27f
530 <532>
2019-02-19 20:51:26 +0800

    d

--- onemachine/res152_dist_main.py
+++ onemachine/res152_dist_main.py
@@ -147,7 +147,27 @@ def train(epoch):
     back_process.join()
 
 
+def train2(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(0), targets.to(1)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
 
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 
 
@@ -194,5 +214,6 @@ def test(epoch):
 
 
 for epoch in range(start_epoch, start_epoch + 200):
-    train(epoch)
+    train2(epoch)
     test(epoch)
+
