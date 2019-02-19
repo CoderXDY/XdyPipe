cd9b8b12d08a69a2f81f2bef4a1d816220ada12d
xdy <444875571@qq.com>
2019-02-19 20:47:37 +0800

    d

--- onemachine/resnet_pipe_model.py
+++ onemachine/resnet_pipe_model.py
@@ -9,8 +9,9 @@ Reference:
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from multiprocessing.dummy import Process, Queue
 
-__all__ = ['ResNet18', 'ResNet34', 'ResNet50', 'ResNet152']
+__all__ = ['ResNet18', 'ResNet34', 'ResNet50', 'ResNet152', 'ResPipeNet18', 'ResPipeNet50']
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -106,17 +107,20 @@ class ResNet(nn.Module):
 
 
 class ResPipeNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, size, wait, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.queue = Queue(size)
+        self.wait = wait
+        with torch.cuda.device(0):
+            self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False).cuda()
+            self.bn1 = nn.BatchNorm2d(64).cuda()
+            self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1).cuda()
+            self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2).cuda()
+        with torch.cuda.device(1):
+            self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2).cuda()
+            self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2).cuda()
+            self.linear = nn.Linear(512*block.expansion, num_classes).cuda()
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -130,11 +134,15 @@ class ResPipeNet(nn.Module):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
+        self.queue.put(out.cuda(1))
+        if self.queue.qsize() > self.wait:
+            out = self.queue.get()
+            out = self.layer3(out)
+            out = self.layer4(out)
+            out = F.avg_pool2d(out, 4)
+            out = out.view(out.size(0), -1)
+            out = self.linear(out)
+            return out
         return out
 
 
@@ -147,8 +155,11 @@ class ResPipeNet(nn.Module):
 
 
 
+def ResPipeNet18(size, wait):
+    return ResPipeNet(size, wait, BasicBlock, [2, 2, 2, 2])
 
-
+def ResPipeNet50(size, wait):
+    return ResPipeNet(size, wait, Bottleneck, [3, 4, 6, 3])
 
 
 
